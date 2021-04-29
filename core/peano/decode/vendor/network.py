0b9589963a4a805a71b88733a9f330fda53e7711
Alexander Demidovskij <monadv@yandex.ru>
2021-04-29 18:11:44 +0300

    all tests are green (!)

--- core/peano/decode/vendor/network.py
+++ core/peano/decode/vendor/network.py
@@ -9,7 +9,7 @@ def build_decode_number_network(fillers, dual_roles, max_depth):
 
     input_num_elements, flattened_tree_num_elements = unshift_matrix(dual_roles[0], filler_len, max_depth - 1).shape
     shape = (flattened_tree_num_elements + filler_len, 1)
-    flattened_input = Input(shape=(*shape,), batch_shape=(*shape,))
+    flattened_input = Input(shape=(*shape,), batch_size=1)
 
     const_inputs, is_not_zero, unshifted = check_if_not_zero_branch(
         decrementing_input=flattened_input,

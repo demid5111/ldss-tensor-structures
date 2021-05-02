4ccfa3fcbce784844d303e887cc99e97e445c9c7
Alexander Demidovskij <monadv@yandex.ru>
2021-05-02 18:01:49 +0300

    refactored to make 2-tuple module more self-contained no. 4

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from core.utils import flattenize_per_tensor_representation
+
+from core import model_2_tuple
--- core/model_2_tuple/__init__.py
+++
@@ -1,6 +0,0 @@
-from .aggregation import aggregate_model_tuples, aggregate_and_check
-from .core import Model2Tuple
-from .decode import decode_model_2_tuple_tpr
-from .encode import encode_model_2_tuple
-
-from core.utils import flattenize_per_tensor_representation

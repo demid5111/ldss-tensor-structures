7e3be68166e0738b69f2dfb0a00b7a6d85954fff
Alexander Demidovskij <monadv@yandex.ru>
2021-05-02 18:03:15 +0300

    refactored to make 2-tuple module more self-contained no. 5

---
+++ core/model_2_tuple/__init__.py
@@ -0,0 +1,4 @@
+from .aggregation import aggregate_model_tuples, aggregate_and_check
+from .core import Model2Tuple
+from .decode import decode_model_2_tuple_tpr
+from .encode import encode_model_2_tuple

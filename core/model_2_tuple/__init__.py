c013052e9503141e95f33c515004185fda4b39b0
Alexander Demidovskij <alexander.demidovskij@intel.com>
2021-07-22 12:08:01 +0300

    [2-TUPLE] All filler vectors are binary (#34)

--- core/model_2_tuple/__init__.py
+++ core/model_2_tuple/__init__.py
@@ -2,3 +2,4 @@ from .aggregation import aggregate_model_tuples, aggregate_and_check
 from .core import Model2Tuple
 from .decode import decode_model_2_tuple_tpr
 from .encode import encode_model_2_tuple
+from .filler_factory import FillerFactory
